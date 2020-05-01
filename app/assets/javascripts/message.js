08aa679a8b2690162001049ffdef768c27fdd78f
Uya16 <ymogashi@gmail.com>
2020-05-01 13:28:18 +0900

    Update message.js

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -71,6 +71,7 @@ $(function() {
 
   var reloadMessages = function() {
     var last_message_id = $('.message-list:last').data("message-id");
+    console.log(last_message_id);
     $.ajax({
       url: "api/messages",
       type: 'get',
